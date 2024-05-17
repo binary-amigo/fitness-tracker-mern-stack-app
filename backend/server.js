43c500063c74745535e1b0a52efbb75720870596
Imran Khan <imrankh8602@gmail.com>
2024-05-17 18:41:20 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ require("dotenv/config");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Hello to Fitness Tracker API");
 });
 mongoose.connect(
-  process.env.MONGO_URI,
+  'mongodb+srv://binary_amigo:webarebears@cluster0.i1nsxes.mongodb.net/Health',
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
   () => console.log("Database connected")
 );
